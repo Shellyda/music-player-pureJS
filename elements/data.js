fb38d6c770543a85bc0488ac947b34311f08e326
Shellyda Barbosa <shellydabarbosa@gmail.com>
2020-12-02 22:52:55 -0200

    update(main): not perfect but that's what i could do <3

--- elements/data.js
+++ elements/data.js
@@ -3,7 +3,7 @@ window.audios = [
 	title: "You Got It",
 	artist: "Vedo",
 	cover: "vedo.jpg",
-	file: "piccolo-and-a-cane.mp3",
+	file: "vedo.mpeg",
 	duration: "03:24"
 },
 {
@@ -16,29 +16,29 @@ window.audios = [
 {
 	title: "Less I Know The Better",
 	artist: "Tame Impala",
-	cover: "m3.png",
-	file: "piccolo-and-a-cane.mp3",
+	cover: "m3.jpg",
+	file: "tame.mpeg",
 	duration: "03:36"
 },
 {
 	title: "Miracles",
 	artist: "Coldplay, Big Sean",
-	cover: "m3.png",
-	file: "piccolo-and-a-cane.mp3",
+	cover: "m4.jpg",
+	file: "col.mpeg",
 	duration: "03:36"
 },
 {
 	title: "Madagascar",
 	artist: "Edy Lemond",
-	cover: "m3.png",
-	file: "piccolo-and-a-cane.mp3",
+	cover: "m5.jpg",
+	file: "mada.mpeg",
 	duration: "03:36"
 },
 {
    	title: "New Light",
 	artist: "John Mayer",
-	cover: "m3.png",
-	file: "piccolo-and-a-cane.mp3",
+	cover: "m6.jpg",
+	file: "new.mpeg",
 	duration: "03:36"
 }
 ]
